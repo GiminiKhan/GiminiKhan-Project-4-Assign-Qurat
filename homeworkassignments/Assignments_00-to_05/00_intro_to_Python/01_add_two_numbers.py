4eb751db766b4e20337c3bc6e610712594b03b45
GiminiKhan <160707616+GiminiKhan@users.noreply.github.com>
2025-04-25 11:03:17 +0500

    Update 01_add_two_numbers.py

--- homeworkassignments/Assignments_00-to_05/00_intro_to_Python/01_add_two_numbers.py
+++ homeworkassignments/Assignments_00-to_05/00_intro_to_Python/01_add_two_numbers.py
@@ -19,13 +19,10 @@
 
 def main():
     print("This program adds two numbers.")
-    num1 : str = input("Enter first number: ")
-    num1 : int = int(num1)
-    num2  : str = input("Enter second number: ")
-    num2 : int = int(num2)
-    total : int = num1 + num2
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    total = num1 + num2
     print("The total is " + str(total) + ".")
-
 
 # This provided line is required at the end of
 # Python file to call the main() function.
