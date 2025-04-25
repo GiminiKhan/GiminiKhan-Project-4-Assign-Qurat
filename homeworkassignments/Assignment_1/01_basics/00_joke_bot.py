34241a3a3ac7d62c386424d4dc703ea3ede8c8fc
GiminiKhan <160707616+GiminiKhan@users.noreply.github.com>
2025-04-25 10:02:12 +0500

    Update 00_joke_bot.py

--- homeworkassignments/Assignment_1/01_basics/00_joke_bot.py
+++ homeworkassignments/Assignment_1/01_basics/00_joke_bot.py
@@ -25,27 +25,23 @@
 
 #What do you want? Joke Here is a joke for you! Panaversity GPT - Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs'
 
-def main():
-    print("Delete this line and write your code here! :)")
-
-
-# This provided line is required at the end of
-# Python file to call the main() function.
-if __name__ == '__main__':
-    main()
-
-    PROMPT: str = "What do you want? "
-JOKE: str = "Here is a joke for you! Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs'"
+# Constants
+PROMPT: str = "What do you want? "
+JOKE: str = "Here is a joke for you! Panaversity GPT - Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs'"
 SORRY: str = "Sorry I only tell jokes."
 
 def main():
-  
-    user_input = user_input.strip().lower()
+    print("This program shows a simple joke bot! :)")
     
-    if "joke" in user_input:
+    user_input = input(PROMPT)
+    
+    if user_input.strip() == "Joke":
         print(JOKE)
     else:
         print(SORRY)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
+
+       
